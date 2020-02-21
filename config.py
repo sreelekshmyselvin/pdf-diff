d58ea206c6f0b99b7be8840df0b8fb72fc0e8dbf
sreelekshmys <sreelekshmys@accubits.com>
2020-02-21 21:13:16 +0530

    Modified project to use as an API

---
+++ config.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 18/02/20 3:18 PM
+@author : Sreelekshmy Selvin
+
+"""
+# how to mark the differences in the two files
+#  here you can mention the style type and color
+
+underline = 'underline,underline'
+box = 'box,box'
+strike = 'strike,strike'
+underline_with_box = 'underline,box'
+stroke = 'stroke'
+strike_with_underline = 'strike,underline'
+
+width = 900
+
+minute_change_marker = "orange"
+new_additions_marker = "green"
+missing_data_marker = "red"
+default_marker = "blure"
+
+STYLES = strike_with_underline
+WIDTH = width
---
+++ pdfdifferencefinder.py
@@ -0,0 +1,23 @@
+#-*- coding: utf-8 -*-
+"""
+Created on 07/02/20 12:17 PM
+@author : Sreelekshmy Selvin
+
+"""
+import differenceanalyser as analyser
+import config
+
+def processPDF(filename1,filename2):
+	styles = config.STYLES
+	style = styles.split(',')
+	result_width = config.WIDTH
+	changes = analyser.compute_changes(filename1, filename2)
+	img = analyser.render_changes(changes, style, result_width)
+	img.save("comparison.png")
+	print("Done")
+
+
+if __name__ == '__main__':
+	filename1 = '1.pdf'
+	filename2 = '2.pdf'
+	processPDF(filename1,filename2)
